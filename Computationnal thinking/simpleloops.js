79b2e4e4dff39eccb4d074a71d45014413d6e304
tjay1760 <mcjthiongo@gmail.com>
2024-01-14 23:47:08 +0300

    correct bug

---
+++ Computationnal thinking/simpleloops.js
@@ -0,0 +1,12 @@
+const compilation = (list) =>{
+
+console.log(list[0])
+mid = (Math.floor(list.length/2))
+midpoint = list[mid]
+console.log(midpoint)
+
+}
+
+
+    
+ compilation(['one','two','three','four','five'])

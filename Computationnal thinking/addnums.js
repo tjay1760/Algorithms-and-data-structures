2c6d1d3201019e020b4c7808ac7e0c9c8583cf91
tjay1760 <mcjthiongo@gmail.com>
2024-01-14 23:09:59 +0300

    evaluating complexity

--- Computationnal thinking/addnums.js
+++ Computationnal thinking/addnums.js
@@ -1,5 +1,6 @@
 const addnums = (num)=>{
-    total = num[0]+num[1];
-return total 
+    total = num[0] /*lookup */ +num[1] /*lookup */; //assignment
+return total // 
 } 
 console.log(addnums([2,4]));
+// complexity is at (O)1

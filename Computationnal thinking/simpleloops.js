d94e04a1e56a0c1e38becf6422abf58e1a778fc3
tjay1760 <mcjthiongo@gmail.com>
2024-01-16 15:24:36 +0300

    CAlculating time complexity

--- Computationnal thinking/simpleloops.js
+++ Computationnal thinking/simpleloops.js
@@ -1,7 +1,7 @@
 const compilation = (list) =>{
 
-console.log(list[0])
-mid = (Math.floor(list.length/2))
+console.log(list[0])// (O)n
+mid = (Math.floor(list.length/2))// (0)n/2
 midpoint = list[mid]
 console.log(midpoint)
 
